8cf3912a44f92e04e4e2f81b3ea4800badf5dd58
Richard Yance <yance0022@gmail.com>
2019-05-15 03:17:17 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  ships = []
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+  end
+
+  def self.all
+    ships
+  end
+
+  def self.clear
+    ships.clear
+  end
+
 end
